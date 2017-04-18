ad9e67134c65ae84d72de14af2348b4f71b7eeba
chenlili <chenlili@pset.suntec.net>
2017-04-18 16:55:59 +0800

    Finish task 7.
    Change-Id: I0d9520febb5fa11c811da9eeb75f7b395abbfde6

--- src/semant/semant.c
+++ src/semant/semant.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "ast.h"
 #include "semant.h"
 
 #define TODO()\
@@ -31,7 +32,7 @@ void Table_insert (Dec_t dec)
   
   if (Table_lookup (dec->id) != -1){
     fprintf (stderr, "Error: the variable "
-	     "\"%s\" has been declared!\n", dec->id);
+	     "\"%s\" is declared duplicately!\n", dec->id);
     exit (0);
   }
   table = List_new (dec, table);
@@ -61,7 +62,52 @@ void check_decs(List_t decs)
 // Your job:
 Type_t check_exp (Exp_t exp)
 {
-  TODO();
+  switch(exp->kind) {
+  case EXP_INT:
+    return TYPE_INT;
+  case EXP_TRUE:
+  case EXP_FALSE:
+    return TYPE_BOOL;
+  case EXP_ID:
+  {
+    Type_t t = Table_lookup(((Exp_Id)exp)->id);
+    if (t == -1) {
+      fprintf (stderr, "Error: the variable "
+	     "\"%s\" has not benn declared!\n", ((Exp_Id)exp)->id);
+      exit(-1);
+    } else {
+      return t;
+    }
+  }
+  case EXP_ADD:
+  case EXP_SUB:
+  case EXP_TIMES:
+  case EXP_DIVIDE:
+  {
+    Exp_Add e = (Exp_Add) exp;
+    Type_t t1 = check_exp(e->left);
+    Type_t t2 = check_exp(e->right);
+    if (t1 == TYPE_INT && t2 == TYPE_INT) {
+      return TYPE_INT;
+    } else {
+      fprintf(stderr, "The type of arithmetic operand must be integer!\n");
+      exit(-1);
+    }
+  }
+  case EXP_AND:
+  case EXP_OR:
+  {
+    Exp_And e = (Exp_And) exp;
+    Type_t t1 = check_exp(e->left);
+    Type_t t2 = check_exp(e->right);
+    if (t1 == TYPE_BOOL && t2 == TYPE_BOOL) {
+      return TYPE_BOOL;
+    } else {
+      fprintf(stderr, "The type of boolean operand must be bool!\n");
+      exit(-1);
+    }
+  }
+  }
 }
 
 ////////////////////////////////////////
@@ -70,7 +116,39 @@ Type_t check_exp (Exp_t exp)
 // Your job:
 void check_stm (Stm_t stm)
 {
-  TODO();
+  switch(stm->kind) {
+  case STM_ASSIGN: {
+    Type_t t1 = Table_lookup(((Stm_Assign)stm)->id);
+    Type_t t2 = check_exp(((Stm_Assign)stm)->exp);
+    if (t1 == -1) {
+      fprintf (stderr, "Error: the variable "
+	     "\"%s\" has not benn declared!\n", ((Stm_Assign)stm)->id);
+      exit(-1);
+    } else if (t1 != t2) {
+      fprintf(stderr, "The type of id and that of expression don't match!\n");
+      exit(-1);
+    }
+    return;
+  }
+  case STM_PRINTI:
+  {
+    Type_t t = check_exp(((Stm_Printi)stm)->exp);
+    if (t != TYPE_INT) {
+      fprintf(stderr, "The type of parameter of printi() should be integer!\n");
+      exit(-1);
+    }
+    return;
+  }
+  case STM_PRINTB:
+  {
+    Type_t t = check_exp(((Stm_Printb)stm)->exp);
+    if (t != TYPE_BOOL) {
+      fprintf(stderr, "The type of parameter of printb() should be bool!\n", t);
+      exit(-1);
+    }
+    return;
+  }
+  }
 }
 
 void check_stms(List_t stms)
@@ -81,7 +159,6 @@ void check_stms(List_t stms)
     stms = stms->next;
   }
   return;
-  TODO();
 }
 
 
